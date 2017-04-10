09a4ffb0749592245d76fbe42d93811cba19c9d1
nathanlogus <nathanpirral@gmail.com>
2017-04-10 17:20:23 -0300

    Upload dos reps

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+
+function enviar(){
+// Lógica de envio da mensagem;
+if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("comentario").value != ""){ 
+alert("Mensagem enviada com sucesso!");
+};
+}
